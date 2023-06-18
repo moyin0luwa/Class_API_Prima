a675eec687164ab7c9b5dec65a62977ff5842278
Moyinoluwalogo <m.oladipsgmail.com>
2023-06-18 18:00:05 +0100

    Commit 02 -Created server -Installed needed dependencies -Intialized sserver setting PORT number

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { json } = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(json());
+app.use(express.urlencoded({extended: true}))
+
+app.get("/api", (req, res) => {
+	res.status(200).json({ message: "Server initialized" });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT}`);
+});
