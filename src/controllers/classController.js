94f636f100cbea9c771c05450049c70cadb57ab8
Moyinoluwalogo <m.oladipsgmail.com>
2023-07-12 22:37:17 +0100

    Commit 05 - Created the controller function to handle enrolling of students to a class - Created the appropriate route for this and linked the controller to the router - Changed some details in the schema of the student to allow for accessing of relations - Tested the new route to enroll students sucessfully

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -18,16 +18,50 @@ const createClass = async (req, res) => {
                 }
             }
         })
-        console.log( lectureRoom)
         return res.status(200).json({
             success: true,
-            message: "Product added successfully",
+            message: "Class created successfully",
             class: lectureRoom
         })
    
 }
 
+const enrollStudent = async (req, res) => {
+    const {subject, studentname, studentage, studentgender, teachername, teacherage, teachergender } = await req.body
+    const student = await prisma.student.create({
+        data: {
+            name: studentname,
+            age: studentage,
+            gender: studentgender, 
+            room_enrolled: {
+                connectOrCreate: {
+                    where: {
+                        subject: subject,
+                    }, 
+                    create: {
+                        subject: subject,
+                        teacher: {
+                            create: {
+                               name: teachername,
+                               age: teacherage,
+                               gender: teachergender   
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+    console.log(student)
+        return res.status(200).json({
+            success: true,
+            message: "Student enrolled successfully",
+            student: student
+        })
+}
+
 
 module.exports = {
     createClass,
+    enrollStudent
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { createClass } = require('../controllers/classController')
+const { createClass, enrollStudent } = require('../controllers/classController')
 
 
 router.route('/createclass').post(createClass)
-
+router.route('/enrollstudent').post(enrollStudent)
 module.exports = router
